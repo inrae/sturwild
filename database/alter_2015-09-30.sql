67083119671e72d11f5b1ff53e752d54e05b6f1c
Eric Quinton <eric.quinton@inrae.fr>
2021-02-23 11:20:57 +0100

    :feat framework: suppression du dossier database

--- database/alter_2015-09-30.sql
+++
@@ -1,9 +0,0 @@
-select setval('sturwild.mime_type_mime_type_id_seq', (select max(mime_type_id) from sturwild.mime_type));
-insert into sturwild.mime_type (content_type, extension) values
-('video/mp4','mp4'),
-('video/x-msvideo','avi'),
-('video/3gpp','3gp'),
-('video/x-ms-wmv','wmv'),
-('video/MP2T','ts');
-
-insert into sturwild.mime_type (content_type, extension) values ('video/quicktime','mov');
